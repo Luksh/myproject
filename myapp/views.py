320c746a619fbac763d9d6e30673fe4096a7e6a7
Luksh <hotmale_luksh@hotmail.com>
2022-05-13 08:04:50 +0545

    word counter

--- myapp/views.py
+++ myapp/views.py
@@ -1,8 +1,20 @@
+from multiprocessing import context
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse('<h1>Laxman Maharjan</h1>')
+    context = {
+        'name': 'Laxman',
+        'age': 25,
+        'sex': 'male',
+        'country': 'Nepal'
+    }
+    return render(request, 'index.html', context)
+
+
+def counter(request):
+    words = request.GET['words']
+    total_words = len(words.split())
+    return render(request, 'counter.html', {'amount': total_words})
