f61878fe2d246f92d620f22889065b14f90ca898
Luksh <hotmale_luksh@hotmail.com>
2022-05-18 19:20:37 +0545

    models

--- myapp/views.py
+++ myapp/views.py
@@ -16,19 +16,7 @@ def index1(request):
 
 
 def index(request):
-    feature1 = Feature()
-    feature1.id = 0
-    feature1.name = 'Laxman Maharjan'
-    feature1.details = 'I am a programmer'
-    feature1.is_true = True
-
-    feature2 = Feature()
-    feature2.id = 1
-    feature2.name = 'Anita Maharjan'
-    feature2.details = 'I am a nurse'
-    feature2.is_true = False
-
-    features = [feature1, feature2]
+    features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
 
 
