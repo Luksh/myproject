abd32b35ed4c407842ae6783206729553704f4d7
Luksh <hotmale_luksh@hotmail.com>
2022-05-24 10:06:54 +0545

    register

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
 from multiprocessing import context
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -18,6 +20,32 @@ def index1(request):
 def index(request):
     features = Feature.objects.all()
     return render(request, 'index.html', {'features': features})
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        rpassword = request.POST['rpassword']
+
+        if password == rpassword:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email alrady exists')
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username alrady exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Passwords do not match')
+            return redirect('register')
+    else:
+        return render(request, 'register.html')
 
 
 def counter(request):
